616cefc6072d0f32c00c333d85c344c4b367834b
johhnwisent-droid <johhnwisent@gmail.com>
2026-08-04 11:20:33 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 const WISENT_VERSION="V199";
-const WISENT_CACHE="wisent-erp-static-v199";
+const WISENT_PATCH="display-fix-1";
+const WISENT_CACHE="wisent-erp-static-v199-fix1";
 const WISENT_CACHE_PREFIX="wisent-erp-static-";
 const WISENT_META_CACHE="wisent-erp-meta-v1";
 const WISENT_STABLE_REQUEST="./__wisent_stable_cache__";
