9ae5338e08e25c46d7c4ee4192dc2b708d3da438
johhnwisent-droid <johhnwisent@gmail.com>
2026-08-06 16:01:07 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const WISENT_VERSION="V226";
-const WISENT_CACHE="wisent-erp-static-v226";
+const WISENT_VERSION="V227";
+const WISENT_CACHE="wisent-erp-static-v227";
 const WISENT_CACHE_PREFIX="wisent-erp-static-";
 const WISENT_META_CACHE="wisent-erp-meta-v1";
 const WISENT_STABLE_REQUEST="./__wisent_stable_cache__";
@@ -153,3 +153,5 @@ self.addEventListener("fetch",event=>{
     }
   })());
 });
+
+// WISENT V227 - novo deploy
