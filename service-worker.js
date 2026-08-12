50a2c9032833f3828eee4aa2928cafb1616c2d7f
johhnwisent-droid <johhnwisent@gmail.com>
2026-08-12 18:30:41 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const WISENT_VERSION="V255";
-const WISENT_CACHE="wisent-erp-static-v255";
+const WISENT_VERSION="V256";
+const WISENT_CACHE="wisent-erp-static-v256";
 const WISENT_CACHE_PREFIX="wisent-erp-static-";
 const WISENT_META_CACHE="wisent-erp-meta-v1";
 const WISENT_STABLE_REQUEST="./__wisent_stable_cache__";
@@ -154,4 +154,4 @@ self.addEventListener("fetch",event=>{
   })());
 });
 
-// WISENT V255 - hierarquia, cores e tipografia do Dashboard Private Label
+// WISENT V256 - tipografia do Financeiro e data atual em novos lançamentos
